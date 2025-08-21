ef44f3c41626e873fede6815aeceeb40853d7ce4
lokesh <lokeshmittakanti17@gmail.com>
2025-08-21 16:38:37 +0530

    Changed static data to dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import DownloadIcon from "@mui/icons-material/Download";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Tooltip } from "@mui/material";
+import { allusers } from "./utils/usersData";
 const App = () => {
   return (
     <div
@@ -89,27 +90,28 @@ const App = () => {
                   borderBottom: "1px solid lightgray",
                   boxShadow: "0px 0px 1px 0px lightgray",
                   height: "60px",
-                  backgroundColor: "#f9fbfc",
                 }}
               >
                 <td>Full Name</td>
                 <td>Payment Methods</td>
                 <td>Category</td>
                 <td>ClickthroughPercentage</td>
-                <td colSpan="3">Actions</td>
+                <td colSpan="3"></td>
               </tr>
             </thead>
             <tbody>
-              <tr
+              {allusers.map((item)=>
+               <tr
+               key={item.Id}
                 style={{
                   borderBottom: "1px solid lightgray",
                   boxShadow: "0px 0px 1px 0px lightgray",
                 }}
               >
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
+                <td>{item.name}</td>
+                <td>{item.paymentType}</td>
+                <td>{item.category}</td>
+                <td>{item.percentageValue}</td>
                 <td>
                   <Tooltip title="Delete">
                     <DeleteOutlineIcon />
@@ -126,235 +128,7 @@ const App = () => {
                   </Tooltip>
                 </td>
               </tr>
-              <tr
-                style={{
-                  borderBottom: "1px solid lightgray",
-                  boxShadow: "0px 0px 1px 0px lightgray",
-                }}
-              >
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
-                <td>
-                  <Tooltip title="Delete">
-                    <DeleteOutlineIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="Download">
-                    <DownloadIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="More">
-                    <MoreVertIcon />
-                  </Tooltip>
-                </td>
-              </tr>
-              <tr
-                style={{
-                  borderBottom: "1px solid lightgray",
-                  boxShadow: "0px 0px 1px 0px lightgray",
-                }}
-              >
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
-                <td>
-                  <Tooltip title="Delete">
-                    <DeleteOutlineIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="Download">
-                    <DownloadIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="More">
-                    <MoreVertIcon />
-                  </Tooltip>
-                </td>
-              </tr>
-              <tr
-                style={{
-                  borderBottom: "1px solid lightgray",
-                  boxShadow: "0px 0px 1px 0px lightgray",
-                }}
-              >
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
-                <td>
-                  <Tooltip title="Delete">
-                    <DeleteOutlineIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="Download">
-                    <DownloadIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="More">
-                    <MoreVertIcon />
-                  </Tooltip>
-                </td>
-              </tr>
-              <tr
-                style={{
-                  borderBottom: "1px solid lightgray",
-                  boxShadow: "0px 0px 1px 0px lightgray",
-                }}
-              >
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
-                <td>
-                  <Tooltip title="Delete">
-                    <DeleteOutlineIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="Download">
-                    <DownloadIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="More">
-                    <MoreVertIcon />
-                  </Tooltip>
-                </td>
-              </tr>
-              <tr
-                style={{
-                  borderBottom: "1px solid lightgray",
-                  boxShadow: "0px 0px 1px 0px lightgray",
-                }}
-              >
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
-                <td>
-                  <Tooltip title="Delete">
-                    <DeleteOutlineIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="Download">
-                    <DownloadIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="More">
-                    <MoreVertIcon />
-                  </Tooltip>
-                </td>
-              </tr>
-              <tr
-                style={{
-                  borderBottom: "1px solid lightgray",
-                  boxShadow: "0px 0px 1px 0px lightgray",
-                }}
-              >
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
-                <td>
-                  <Tooltip title="Delete">
-                    <DeleteOutlineIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="Download">
-                    <DownloadIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="More">
-                    <MoreVertIcon />
-                  </Tooltip>
-                </td>
-              </tr>
-              <tr
-                style={{
-                  borderBottom: "1px solid lightgray",
-                  boxShadow: "0px 0px 1px 0px lightgray",
-                }}
-              >
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
-                <td>
-                  <Tooltip title="Delete">
-                    <DeleteOutlineIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="Download">
-                    <DownloadIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="More">
-                    <MoreVertIcon />
-                  </Tooltip>
-                </td>
-              </tr>
-              <tr
-                style={{
-                  borderBottom: "1px solid lightgray",
-                  boxShadow: "0px 0px 1px 0px lightgray",
-                }}
-              >
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
-                <td>
-                  <Tooltip title="Delete">
-                    <DeleteOutlineIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="Download">
-                    <DownloadIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="More">
-                    <MoreVertIcon />
-                  </Tooltip>
-                </td>
-              </tr>
-              <tr>
-                <td>AliceSmith</td>
-                <td>VISA</td>
-                <td>Arts Business Travel</td>
-                <td>40%</td>
-                <td>
-                  <Tooltip title="Delete">
-                    <DeleteOutlineIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="Download">
-                    <DownloadIcon />
-                  </Tooltip>
-                </td>
-                <td>
-                  <Tooltip title="More">
-                    <MoreVertIcon />
-                  </Tooltip>
-                </td>
-              </tr>
+              )}
             </tbody>
           </table>
         </div>
--- src/utils/usersData.js
+++ src/utils/usersData.js
@@ -1,41 +1,74 @@
-// export const allusers=[
-// {
-//         id:1,
-//         name:'',
-//         subName:'',
-//         paymentType:'',
-//         accountNumber:'',
-//         category:['','',''],
-//         percentageValue:''
-    
-//     },
-//     {
-//         id:2,
-//         name:'',
-//         subName:'',
-//         paymentType:'',
-//         accountNumber:'',
-//         category:['','',''],
-//         percentageValue:''
-    
-//     },
-//     {
-//         id:3,
-//         name:'',
-//         subName:'',
-//         paymentType:'',
-//         accountNumber:'',
-//         category:['','',''],
-//         percentageValue:''
-    
-//     },{
-//         id:4,
-//         name:'',
-//         subName:'',
-//         paymentType:'',
-//         accountNumber:'',
-//         category:['','',''],
-//         percentageValue:''
-    
-//     }
-// ]
+export const allusers = [
+
+  {
+    Id: 1,
+    name: "Lokesh",
+    paymentType: "VISA",
+    category: "awscds",
+    percentageValue: "40%",
+  },
+  {
+    Id: 2,
+    name: "Vinay",
+    paymentType: "MASTER CARD",
+    category: "qascdce",
+    percentageValue: "70%",
+  },
+  {
+    Id: 3,
+    name: "Harsha",
+    paymentType: "VISA",
+    category: "asdvf",
+    percentageValue: "90%",
+  },
+  {
+    Id: 4,
+    name: "venky",
+    paymentType: "MASTER CARD",
+    category: "asdfg",
+    percentageValue: "30%",
+  },
+  {
+    Id: 5,
+    name: "Nani",
+    paymentType: "MASTER CARD",
+    category: "wdsrf",
+    percentageValue: "40%",
+  },
+  {
+    Id: 6,
+    name: "Chintu",
+    paymentType: "MASTER CARD",
+    category: "wdsfes",
+    percentageValue: "20%",
+  },
+  {
+    Id: 7,
+    name: "Bunny",
+    paymentType: "MASTER CARD",
+    category: "gvbhj",
+    percentageValue: "100%",
+  },
+  {
+    Id: 8,
+    name: "Bantu",
+    paymentType: "MASTER CARD",
+    category: "detfg",
+    percentageValue: "30%",
+  },
+  {
+    Id: 9,
+    name: "Chanty",
+    paymentType: "MASTER CARD",
+    category: "wdertf",
+    percentageValue: "45%",
+  },
+  {
+    Id: 10,
+    name: "Nattu",
+    paymentType: "MASTER CARD",
+    category: "wdftg",
+    percentageValue: "55%",
+  },
+
+];
