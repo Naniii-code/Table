e61a8d8d371dbbc57b636a99d949f359d008a270
lokesh <lokeshmittakanti17@gmail.com>
2025-08-22 13:32:15 +0530

    Added delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import DownloadIcon from "@mui/icons-material/Download";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Tooltip } from "@mui/material";
 import { allusers } from "./utils/usersData";
+import { useState } from "react";
 const App = () => {
+  const [users, setUsers] = useState(allusers);
+  const handleDeleteAction = (id) => {
+    const usersData = users.filter((item) => item.Id !== id);
+    setUsers(usersData);
+  };
   return (
     <div
       style={{
@@ -26,57 +32,68 @@ const App = () => {
             >
               Your Customers
             </h3>
-            <button
-              style={{
-                width: "50px",
-                height: "20px",
-                border: "1px solid #4f3fcb",
-                borderRadius: "10px",
-                backgroundColor: "white",
-                color: "#4f3fcb",
-                marginTop: "20px",
-                marginRight: "200px",
-                cursor: "pointer",
-              }}
-            >
-              • New
-            </button>
+
+            <Tooltip title="New">
+              <button
+                className="newBtn"
+                style={{
+                  width: "50px",
+                  height: "20px",
+                  border: "1px solid #4f3fcb",
+                  borderRadius: "10px",
+                  backgroundColor: "white",
+                  color: "#4f3fcb",
+                  marginTop: "20px",
+                  marginRight: "200px",
+                  cursor: "pointer",
+                }}
+              >
+                New
+              </button>
+            </Tooltip>
           </div>
           <div className="btnContainer">
-            <button
-              style={{
-                width: "80px",
-                height: "30px",
-                border: "1px solid lightgray",
-                borderRadius: "10px",
-                cursor: "pointer",
-              }}
-            >
-              Filter
-            </button>
-            <button
-              style={{
-                width: "80px",
-                height: "30px",
-                border: "1px solid lightgray",
-                borderRadius: "10px",
-                cursor: "pointer",
-              }}
-            >
-              Settings
-            </button>
-            <button className="btn"
-              style={{
-                width: "80px",
-                height: "30px",
-                border: "1px solid lightgray",
-                borderRadius: "10px",
-                color: "white",
-                cursor: "pointer",
-              }}
-            >
-              AddUser
-            </button>
+            <Tooltip title="Filter">
+              <button
+                style={{
+                  width: "80px",
+                  height: "30px",
+                  border: "1px solid lightgray",
+                  borderRadius: "10px",
+                  cursor: "pointer",
+                }}
+              >
+                Filter
+              </button>
+            </Tooltip>
+            <Tooltip title="Settings">
+              <button
+                style={{
+                  width: "80px",
+                  height: "30px",
+                  border: "1px solid lightgray",
+                  borderRadius: "10px",
+                  cursor: "pointer",
+                }}
+              >
+                Settings
+              </button>
+            </Tooltip>
+            <Tooltip title="AddUser">
+              <button
+                className="btn"
+                style={{
+                  width: "80px",
+                  height: "30px",
+                  border: "1px solid lightgray",
+                  borderRadius: "10px",
+                  color: "white",
+                  cursor: "pointer",
+                }}
+              >
+                AddUser
+              </button>
+            </Tooltip>
           </div>
         </div>
         <div className="mainSection">
@@ -97,22 +114,38 @@ const App = () => {
               </tr>
             </thead>
             <tbody>
-              {allusers.map((item) => (
-                <tr
+              {users.map((item) => (
+                <tr 
                   key={item.Id}
                   style={{
                     borderBottom: "1px solid lightgray",
                     boxShadow: "0px 0px 1px 0px lightgray",
+                    height:'44.5px',
                   }}
                 >
-                  <td style={{fontWeight:'bold',color:'#81888c'}}> {item.name}<br />
-                      <span style={{color:'#8c8e8f'}}>{item.subName}</span></td>
-                  <td style={{color:'#81888c'}}>{item.paymentType}</td>
-                  <td  style={{color:'#81888c'}}>{item.category}</td>
-                  <td  style={{color:'#81888c'}}>{item.percentageValue}</td>
+                  <td style={{ fontWeight: "bold", color: "#81888c" }}>
+                    {" "}
+                    {item.name}
+                    <br />
+                    <span style={{ color: "#8c8e8f", fontWeight: "lighter" }}>
+                      {item.subName}
+                    </span>
+                  </td>
+                  <td style={{ color: "#81888c" }}>{item.paymentType}</td>
+                  <td style={{ color: "#81888c" }}>{item.category}</td>
+                  <td style={{ color: "#81888c" }}>{item.percentageValue}</td>
                   <td>
                     <Tooltip title="Delete">
-                      <DeleteOutlineIcon />
+                      <button
+                        style={{
+                          border: "none",
+                          cursor: "pointer",
+                          backgroundColor: "white",
+                        }}
+                        onClick={() => handleDeleteAction(item.Id)}
+                      >
+                        <DeleteOutlineIcon />
+                      </button>
                     </Tooltip>
                   </td>
                   <td>
@@ -135,4 +168,4 @@ const App = () => {
   );
 };
 
-export default App
+export default App;
